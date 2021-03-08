c9cc6a391f0b1598ec64ccde4aed17c092a029c6
GarethwRite <50506515+GarethwRite@users.noreply.github.com>
2021-03-08 13:53:57 +1300

    Initial commit

---
+++ Source/BuildingEscape/BuildingEscape.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BuildingEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildingEscape, "BuildingEscape" );
+
+
+//Start of game code
